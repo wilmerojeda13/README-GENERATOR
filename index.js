57f8f29eb6469515364b80514e29fcf25bc502b2
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-02-21 17:53:27 -0500

    add created questions for the user input

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project',
-    },
-    {
-        type: 'input',
-        name: 'about',
-        message: 'What the project is about',
-    },
-    {
-        type: 'input' ,
-        name: 'license' ,
-        message: 'what license does your project used' ,
-        choices: ['Milt', 'GLPv3' , 'Glp'] ,
-    },
-    {
-       type : 'input',
-        name: 'general',
-        message: 'General information about project', 
+const fs = require ('fs') ;
 
+//Create Question for user input
+inquirer.
+prompt =([
+  //Title of the project
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of the project?',
+  },
+  //Description of the project
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Description of the project?',
+  },
+  //Installation instruction of the project
+  {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install the project?',
     },
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers)
-  })
+    //Usage Information
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you used the project?',
+  },
+  //License available
+  {
+    type: 'input',
+    name: 'licenses',
+    message: 'Select a license for you project?',
+    choices:['MIT' ,'Apache',  'GPL v3.0', 'None',],
+  },
+  //Contributors for the project
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How to contribute to the project?',
+  },
+  //Test Instructions
+  {
+    type: 'input',
+    name: 'Test',
+    message: 'How to test the project?',
+  },
+  //Github Username
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your Github username?',
+  },
+  //Email adress
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email if you have any questions about the project?',
+  },
+  
+])
+
+
+  

fdfc5e810f90f4f638488fd1804c6b271a73b6c7
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-02-21 22:45:19 -0500

    updating information

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,32 +1,33 @@
 //Function that creates a generateMarkdown for README file
 function generateMarkdown(data) {
-    return `# ${data.title}
-
-    ## License:
-    [![license]((https://img.shields.io/badge/license-${data.license}-blue.svg)]
-    ## Table of Contents
-    -[Description](#description)
-    -[Installation](#installation)
-    -[Usage](#usage)
-    -[Contribution](#description)
-    -[Testing](#description)
-    -[Additonal Information](#additional-information)
+return `# ${data.title}
+## License:
+![Licenses](https://img.shields.io/badge/License-${data.licenses}-yellow.svg)c
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Additonal Information](#additional-information)
+- 
     
-    ## Description
-    ${data.description}
-    ##Installation
-    ${data.installation}
-    ## Usage:
-    ${data.usage}
-    ##License:
-    ${data.license}
-    ##Contribution
-    ${data.contribution}
-    ## Testing
-    ${data.test}
-    ## Contact Information:
-    - Github: [${data.github}](https://github.com/wilmerojeda13)${data.github})
-    - Email: [${data.email}](ojedawilmerantonio@gmail.com) `;
+${data.description}
+## Description:
+${data.installation}
+##Installation:
+${data.usage}
+## Usage:
+${data.usage}
+##License:
+${data.licenses}
+##Contribution:
+${data.contribution}
+## Testing:
+${data.test}
+## Contact Information:
+- Github: [${data.username}](https://github.com/${data.username})
+## Contact Information:
+- Email: [${data.email}](ojedawilmerantonio@gmail.com) `;
 }
-
 module.exports = generateMarkdown
